9ae5bd49f1ca91a2d09d6d1c53885ed1847f6b5c
Anthony Shalagin <anthony.shalagin@gmail.com>
2017-06-15 11:43:06 -0400

    Added a few html pages and used handlebars for dynamic variables

--- server.js
+++ server.js
@@ -6,21 +6,20 @@ var app = express();
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-    // res.send('<h1>Hello Express!</h1>');
-
-    res.send({
-        name: 'Anthony',
-        likes: [
-            'food', 'basketball'
-        ]
+app.get('/', (req,res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome!'
     });
-
 });
 
 
+
 app.get('/about', (req,res) => {
-    res.render('about.hbs');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad', (req,res) => {
